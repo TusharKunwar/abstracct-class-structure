889f1fa71e9d0ead90afa0e0e7c0cd579b4e1e63
TusharKunwar <tusharkunwar993@gmail.com>
2024-12-21 19:00:56 +0545

    Add project files.

---
+++ ConsoleApp1/FileName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab_3;
+
+namespace lab_3
+{
+    public class Human
+    {
+        public int no_of_finger;
+        public string FoodType;
+
+        //constructor
+        public Human(int noofFinger)
+        {
+            no_of_finger = noofFinger;
+        }
+
+        public void print_no_of_finger()
+        {
+            Console.WriteLine("No. of finger:" + no_of_finger);
+        }
+
+        public void DisplayBehaviour()
+        {
+            Console.WriteLine("He is " + FoodType);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// creating objects
+Human ban = new Human(10);
+// ban.no_of_finger = 10;
+ban.print_no_of_finger();
+ban.FoodType = "NON-VEG";
+ban.DisplayBehaviour();
